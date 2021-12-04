9ad89ff5511f6b29a09b07df091023f16db45661
Roman_Zhukov <romazhuk0701@gmail.com>
2021-12-05 01:22:32 +0200

    lab3 done

--- data_service.py
+++ data_service.py
@@ -4,8 +4,8 @@
 # читання файла table1
 def get_price():
     """читання файла 'table1'
-    та формування списку цін
-    повертає список цін
+    та формування списку товарообігу
+    повертає список товарообігу
     """
 
     # накопичення даних файлу у списку
@@ -50,53 +50,53 @@ def get_group():
     return groups_splitted 
 
 
-# вивід списку цін
+# вивід списку товарообігу
 def show_prices(prices):
-    """виводить список клієнтів по заданому інтервалу кодів
+    """виводить інформацію по заданому інтервалу кодів
     """
     # задати інтервал кодів
-    price_code_from = int(input('З якого кода ціни? '))
-    price_code_to = int(input('До якого кода ціни? '))
+    price_code_from = int(input('З якого кода товарної групи? '))
+    price_code_to = int(input('До якого кода товарної групи? '))
 
 
-    # відбір списку цін 
+    # відбір списку  
     filtered_prices = []
     for price in prices:
         if price_code_from <= price[0] <= price_code_to:
             filtered_prices.append(price)
 
     if len(filtered_prices) == 0:
-        print('В списку цін нема таких кодів')
+        print('В списку нема таких кодів')
         return
 
 
     # вивід списку
-    print('СПИСОК ЦІН')
+    print('Товарообіг універмагу')
     for price in filtered_prices:
-        print(f'код товарної групи: {price[0]:3}  план: {price[1]:20} очікуєме виконання: {price[2]:25} рік{price[3][:-1]:30}')
+        print(f'код товарної групи: {price[0]:3}  план: {price[1]:10} очікуєме виконання: {price[2]:10} рік:{price[3][:-1]:10}')
 
 # вивід списку товарних груп
 def show_groups(groups):
-    """виводить список клієнтів по заданому інтервалу кодів
+    """виводить список товарних груп по заданому інтервалу кодів
     """
     # задати інтервал кодів
     group_code_from = int(input('З якого кода товарної групи? '))
     group_code_to = int(input('До якого кода товарної групи? '))
 
 
-    # відбір списку цін 
+    # відбір списку  
     filtered_groups = []
     for group in groups:
         if group_code_from <= group[0] <= group_code_to:
             filtered_groups.append(group)
 
     if len(filtered_groups) == 0:
-        print('В списку цін нема таких кодів')
+        print('В списку нема таких кодів')
         return
 
 
     # вивід списку
-    print('СПИСОК ЦІН')
+    print('Довідник товарних груп')
     for group in filtered_groups:
         print(f'код товарної групи: {group[0]:3} найменування тов групи: {group[1]:20} торгова знижка: {group[2][:-1]:25}')
 
@@ -107,4 +107,4 @@ if __name__ == '__main__':
     show_prices(prices)
     show_groups(groups)
     
-    pass
+    pass  
